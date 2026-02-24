44f24fbd9887e6b8ca65171230e12da38ff3e828
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2026-02-24 22:15:52 +0200

    Inception

---
+++ src/main/java/online/app/eracodes/iownservice/IownServiceApplication.java
@@ -0,0 +1,13 @@
+package online.app.eracodes.iownservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IownServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IownServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/app/eracodes/iownservice/IownServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.app.eracodes.iownservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IownServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
